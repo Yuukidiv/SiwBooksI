e1b520c3784728c9cc15c4bf0ac5420f208d1d45
Yuuki <yuukigpatriarca@gmail.com>
2025-05-29 22:08:07 +0200

    Review now has The name of the User

--- src/main/java/it/uniroma3/siw/controller/ReviewController.java
+++ src/main/java/it/uniroma3/siw/controller/ReviewController.java
@@ -1,6 +1,9 @@
 package it.uniroma3.siw.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,8 +12,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import it.uniroma3.siw.model.Book;
+import it.uniroma3.siw.model.Credentials;
 import it.uniroma3.siw.model.Review;
+import it.uniroma3.siw.model.User;
 import it.uniroma3.siw.service.BookService;
+import it.uniroma3.siw.service.CredentialsService;
 import it.uniroma3.siw.service.ReviewService;
 
 @Controller
@@ -20,6 +26,8 @@ public class ReviewController {
 	private ReviewService reviewService;
 	@Autowired
 	private BookService bookService;
+	@Autowired
+	private CredentialsService credentialsService;
 	
 	
 	@GetMapping("/book/addReview/{id}")
@@ -29,10 +37,22 @@ public class ReviewController {
 		return "review.html";
 	}
 	
+	
+	// called by review.html need to also pass the user I guess
 	@PostMapping("/review")
 	public String addReview(Model model, Review review, @RequestParam("bookId") Long bookId) {
+		// mi serve l'utente per calcolarlo oppure dovrei passarlo in qualche modo
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+	    UserDetails userDetails = (UserDetails) auth.getPrincipal();
+	    Credentials cred = credentialsService.getCredentials(userDetails.getUsername());
+	    User user = cred.getUser();
+	    
+		
+		
 		Book book = this.bookService.getBookById(bookId);
 		review.setReviewedBook(book);
+		review.setReviewedByUser(user);
+		
 		
 		this.reviewService.save(review);
 		return "redirect:/book/" + bookId;
@@ -44,6 +64,7 @@ public class ReviewController {
 	public String deleteReview(Model model, @PathVariable("id") Long id, @RequestParam("bookId") Long bookId) {
 		Review r = this.reviewService.getReviewById(id);
 		this.reviewService.delete(r);
+		
 		return "redirect:/book/" + bookId;
 		
 	}
